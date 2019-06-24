ae62294f55813d28117d8bafda44d3d50e139a9c
Zac Hobbs <zac.hobbs@gmail.com>
2019-06-24 12:10:51 +0100

    reviews logic added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails do
-    resources :doses, only: [:index, :new, :create]
+    resources :doses, only: [:index, :new, :create, :edit]
+    resources :reviews, only: [:new, :create, :destroy]
   end
   resources :doses, only: [:destroy]
   # resources :ingredients
@@ -12,5 +13,6 @@ Rails.application.routes.draw do
 
   root to: "cocktails#index"
 
+  # resources :reviews
   # get "favorites_controller/:cocktail_id" => "favorites#create", :as => :favorite
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_23_111400) do
+ActiveRecord::Schema.define(version: 2019_06_23_213125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,17 @@ ActiveRecord::Schema.define(version: 2019_06_23_111400) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "stars"
+    t.text "description"
+    t.bigint "cocktail_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cocktail_id"], name: "index_reviews_on_cocktail_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -74,4 +85,6 @@ ActiveRecord::Schema.define(version: 2019_06_23_111400) do
   add_foreign_key "doses", "ingredients"
   add_foreign_key "favorites", "cocktails"
   add_foreign_key "favorites", "users"
+  add_foreign_key "reviews", "cocktails"
+  add_foreign_key "reviews", "users"
 end
